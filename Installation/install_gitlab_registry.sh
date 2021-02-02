5f83868ec3af276c724dc4d839dc7645d066f47f
tgbvn <tgbaovn@gmail.com>
2021-02-02 18:11:33 +0700

    Cai dat

--- Installation/install_gitlab_registry.sh
+++ Installation/install_gitlab_registry.sh
@@ -2,7 +2,5 @@
 mkdir -p /var/gitlab/config
 mkdir -p /var/gitlab/logs
 mkdir -p /var/gitlab/data
-mkdir /var/registry
-chown -R 1000:1000 /var/gitlab
-chown -R 1000:1000 /var/registry
-docker-compose -d -f gitlab-regitry_Docker-compose.yml up
+mkdir /var/docker_registry
+docker-compose -f gitlab-regitry_Docker-compose.yml up -d
--- Installation/install_jenkins.sh
+++ Installation/install_jenkins.sh
@@ -2,4 +2,4 @@
 
 mkdir -p /var/jenkins_home
 chown -R 1000:1000 /var/jenkins_home/
-docker-compose -d -f jenkins_Docker-compose.yml up
+docker-compose -f jenkins_Docker-compose.yml up -d
